5fec8d8147a6fad1f4cb2a81fc201eec851a924a
tdeeb <thomasdeeb1@gmail.com>
2016-12-21 22:31:39 -0800

    Added simple sprite animation and a Mario spritesheet
    -Added a FacingDirections enum in PlayerMovement and set it based on the directions the character is moving

---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple player movement class.
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    public enum FacingDirections
+    {
+        North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest
+    }
+
+    /// <summary>
+    /// Character speed.
+    /// </summary>
+    public float Speed = .1f;
+
+    /// <summary>
+    /// The direction the character is facing. This determines the sprites the character will use.
+    /// </summary>
+    public FacingDirections FacingDirection = FacingDirections.South;
+
+    private void Update()
+    {
+        Vector3 diff = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            diff.y = Speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            diff.y = -Speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            diff.x = -Speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            diff.x = Speed;
+        }
+
+        FacingDirection = SetDirectionFromSpeed(new Vector2(diff.x, diff.y));
+
+        transform.position += diff;
+    }
+
+    private FacingDirections SetDirectionFromSpeed(Vector2 speed)
+    {
+        //Moving left
+        if (speed.x < 0)
+        {
+            if (speed.y < 0) return FacingDirections.SouthWest;
+            else if (speed.y > 0) return FacingDirections.NorthWest;
+            return FacingDirections.West;
+        }
+        //Moving right
+        else if (speed.x > 0)
+        {
+            if (speed.y < 0) return FacingDirections.SouthEast;
+            if (speed.y > 0) return FacingDirections.NorthEast;
+            return FacingDirections.East;
+        }
+
+        //Moving down
+        if (speed.y < 0) return FacingDirections.South;
+
+        //Moving up
+        if (speed.y > 0) return FacingDirections.North;
+
+        //Return the current direction if not moving
+        return FacingDirection;
+    }
+}
---
+++ Assets/Scripts/Utility/Sprite/SpriteAnimation.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simple sprite animation class.
+/// This should be used as a base and be extended with other functionality (namely Characters, as they use different sprites for each direction)
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+[DisallowMultipleComponent]
+public class SpriteAnimation : MonoBehaviour
+{
+    /// <summary>
+    /// The array of sprites.
+    /// </summary>
+    public Sprite[] Sprites;
+
+    /// <summary>
+    /// How many frames to display each Sprite.
+    /// This sets the rate at which the animation plays.
+    /// </summary>
+    public int FramesPerSprite;
+
+    /// <summary>
+    /// The sprite renderer. Retrieved on Awake()
+    /// </summary>
+    protected SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// Tracks when the next sprite should be displayed.
+    /// </summary>
+    protected int FrameTracker;
+
+    /// <summary>
+    /// The current index of the sprite being rendered.
+    /// </summary>
+    protected int CurIndex;
+
+    /// <summary>
+    /// Gets the current sprite being displayed.
+    /// </summary>
+    protected Sprite CurSprite { get { return Sprites[CurIndex]; } }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        CurIndex = 0;
+        FrameTracker = Time.frameCount + FramesPerSprite;
+
+        if (Sprites == null || Sprites.Length == 0)
+        {
+            Debug.LogError("There are no sprites to animate!");
+        }
+        else
+        {
+            spriteRenderer.sprite = CurSprite;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //No sprites to display, return
+        if (Sprites == null || Sprites.Length == 0) return;
+
+        //Check if the sprite has been up for enough frames
+        if (Time.frameCount >= FrameTracker)
+        {
+            //Update the index and wrap around
+            CurIndex++;
+            CurIndex %= Sprites.Length;
+
+            //Update the sprite rendered
+            spriteRenderer.sprite = CurSprite;
+
+            //Update the frame count
+            FrameTracker = Time.frameCount + FramesPerSprite;
+        }
+    }
+}
