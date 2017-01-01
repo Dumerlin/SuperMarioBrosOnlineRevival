2a2b0539bc154e8ae7f9e1864fecfd50c0e336aa
tdeeb <thomasdeeb1@gmail.com>
2017-01-01 00:36:22 -0500

    Added menu to open the design doc and added .csproj and .sln files to .gitignore

---
+++ Assets/Scripts/Editor/SMBOResourcesEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class SMBOResourcesEditor
+{
+    [MenuItem("SMBO/Open Design Doc")]
+    public static void OpenDesignDoc()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1tyGA_aW-nBEJDy7ZwRpncU4XDI4A9s6xDVacchs9-Eo");
+    }
+}
